11e0ff39fe30c1520f76d0571eac6ba9e436ac2b
victor-work <vmenzhik132f@gmail.com>
2025-05-28 19:11:55 +0500

    first commit

---
+++ homeWorkOOP.js
@@ -0,0 +1,70 @@
+let inventory = [
+    { id: 1, name: "Apple", price: 0.50, quantity: 200 },
+    { id: 2, name: "Banana", price: 0.30, quantity: 150 },
+    { id: 3, name: "Cherry", price: 0.90, quantity: 50 }
+]
+// 1. Отображение названий продуктов Получите все продукты и вывести их названия на консоль.
+// первый способ
+for(let product of inventory) {
+    console.log("1 task -", product.name)
+}
+// второй способ - "деструктурирование во время обхода"
+//  for(let {name: n} of inventory) {
+//      console.log("1 task -", n)
+//  }
+// -------------------
+
+// 2. Обновление инвентаря Напишите функцию updateInventory(productId, amount),
+// которая обновляет количество определенного ID товара.
+// Количество может быть положительной (добавление к инвентарю)
+// или отрицательной (вычитание из инвентаря).
+// Убедитесь, что функция не позволяет инвентарю опуститься ниже 0.
+
+function updateInventory(productId, amount) {
+    const product = inventory.find(item => item.id === productId);
+    if (product) {
+        const newQuantity = product.quantity + amount;
+
+        if (newQuantity >= 0) {
+            product.quantity = newQuantity;
+            console.log("2 task -", product)
+        } else console.log("В инвентаре недостаточное количество выбранного продукта");
+
+    } else console.log("2 task - Продукт не найден")
+}
+
+updateInventory(3, -30)
+//----------------------
+
+// 3.1 Поиск товаров с низким запасом Напишите функцию findLowStock(threshold),
+// которая возвращает массив названий товаров, количество которых ниже определенного порога.
+
+function findLowStock(threshold) {
+    const productList = inventory.filter(item => item.quantity < threshold);
+
+    for(let product of productList) {
+        console.log("3.1 task -", product.name)
+    }
+}
+
+findLowStock(190)
+
+// 3.2 Меняем на объект и теперь нужно выполнить используя Object.entries()
+// для работы с ключом и данными каждого товара.
+
+let inventory2 = {
+    Apple: 200,
+    Banana: 150,
+    Cherry: 50
+}
+
+function findLowStockObj(threshold){
+    Object.entries(inventory2).forEach(([name, quantity]) => {
+        if(quantity < threshold) console.log("3.2 task -", name);
+    })
+}
+
+findLowStockObj(190)
+
+//Вычислить общую стоимость запасов Напишите функцию totalInventoryValue(),
+// которая вычисляет и возвращает общую стоимость запасов.
---
+++ lessonAnton.js
@@ -0,0 +1,67 @@
+// const people = [
+//     { name: "Alice", lastName: "Cooper", age: 25 },
+//     { name: "Bob", lastName: "Pinger", age: 30 },
+//     { name: "Charlie", lastName: "Davinson", age: 35 }
+// ]
+
+// for (let person of people){
+//     if(person.age <= 30){
+//         result.push(person.age)
+//     }
+// }
+
+// const res = people.filter((person) => {
+//     return person.age <= 30
+// })
+
+// const res = people
+//     .filter(person => person.name === "Charlie")
+//     .map(person=> person.name + ' ' + person.lastName)
+//
+// console.log(res)
+
+
+// let num = 143567
+// let chapter = 1423
+//
+// if(num.toString())
+// console.log()
+
+// const person = {id:123, name:"John", lastName: "Cooper", age: 25};
+
+// console.log(Object.keys(person));
+
+// const heightAndWeight = [180, 80]
+
+// const height = heightAndWeight[0]
+// const width = heightAndWeight[1]
+// const [height, width] = [180 ,80];
+// console.log(height, width)
+
+// Object.entries(person).forEach(([key, value]) => {
+//     console.log('Key: ', key, 'Value: ', value);
+// })
+
+// const {id, name} = person
+//
+// console.log(id, name)
+
+// const a = [1, 2, 3]
+
+// console.log(...a)
+
+// const b = [4, 5 , 6]
+
+// console.log([...a, ...b, 515])
+//
+// console.log({
+//     ...person,
+//     address: 'Almaty'
+// })
+
+// function prigArgs(...args) {
+//     console.log(args)
+// }
+//
+// prigArgs(1, 2, 3, 14, 4535, 53, 2323, 324, 5353, 64, 5)
+
